8f4b56cb2a09e486ade859da6d3c37aab48c7415
minassa20j26-cyber <minassa20j26@gmail.com>
2026-05-29 18:40:05 +0100

    Create main.jsx

---
+++ src/src/main.jsx
@@ -0,0 +1,9 @@
+jsximport React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './zakii-thakii-final-1'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
