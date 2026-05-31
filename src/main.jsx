057f80bb85e0718eedc7ef4e0628f8acdb1ea09d
minassa20j26-cyber <minassa20j26@gmail.com>
2026-05-31 19:25:56 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+jsximport React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import App from './zakii-thakii-final-1'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 )
